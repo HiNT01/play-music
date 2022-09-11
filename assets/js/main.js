dbfe639e559c4814734817f88d7f7e6314590b4b
NGUYEN TRUNG HIEU <hieunt8901@gmail.com>
2022-09-12 00:58:21 +0700

    add folder

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+// songs
+const songs = [
+    {
+        name :'Tòng phu',
+        singer : 'Keyo',
+        path: 'https://mp3-320s1-m-zmp3.zmdcdn.me/a7d1d213ee53070d5e42/2981457658227013023?authen=exp=1663090670~acl=/a7d1d213ee53070d5e42/*~hmac=c93ff7255f4a1302057c7a09356e20ad',
+        image : 'https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_webp/cover/d/f/9/b/df9b187a2b0e565ebe5b6bd60bdef622.jpg',
+    },
+    {
+        name :'',
+        singer : '',
+        path: '',
+        image : '',
+    }
+]
